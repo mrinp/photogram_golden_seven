aeb820f9301978b6e13e76732b02b05189ae6f2e
mrinp <mrinp@users.noreply.github.com>
2017-05-03 10:19:36 -0500

    Closing out old changes

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,46 @@
+class PhotosController < ApplicationController
+
+  def new_form
+    render ("photos/new_form.html.erb")
+  end
+
+  def create_row
+    url = params[:user_url]
+    cap = params[:user_caption]
+    new_photo = Photo.new
+    new_photo.source = url
+    new_photo.caption = cap
+    new_photo.save
+    # render ("photos/create_row.html.erb")
+    redirect_to("/photos/#{new_photo.id}")
+  end
+
+  def index
+    @list_of_all_my_photos = Photo.all
+    render ("photos/index.html.erb")
+  end
+
+  def show
+    @my_photo = Photo.find(params["the_id"])
+    render ("photos/show.html.erb")
+  end
+
+  def edit_form
+    @my_photo = Photo.find(params[:la_id])
+    render ("photos/edit_form.html.erb")
+  end
+
+  def update_row
+    my_photo = Photo.find(params[:le_id])
+    my_photo.source = params[:user_url]
+    my_photo.caption = params[:user_caption]
+    my_photo.save
+    # render ("photos/update_row.html.erb")
+    redirect_to("/photos/#{my_photo.id}")
+  end
+
+  def destroy_row
+    render ("photos/destroy_row.html.erb")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # CREATE
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
+
+  # READ
+  # get ("/", render ("photos/index.html.erb"))
+  get("/photos", { :controller => "photos", :action => "index" })
+  get("/photos/:the_id", { :controller => "photos", :action => "show" })
+
+  # UPDATE
+  get("/photos/:la_id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:le_id", { :controller => "photos", :action => "update_row" })
+
+  # DELETE
+  get("/delete_photo/:da_id", { :controller => "photos", :action => "destroy_row" })
+
 end
